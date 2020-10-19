3f083fbb42cbe67dbf26a38f629848ece79bc900
renataapmonteiro <renataapmonteiro@gmail.com>
2020-10-19 11:57:38 -0300

    fixes and continuation on allegro

--- Tp_Allegro/invaders.c
+++ Tp_Allegro/invaders.c
@@ -3,3 +3,92 @@
 
 const int SCREEN_W = 960;
 const int SCREEN_H = 540;
+
+const float FPS = 100;
+
+int main()
+{
+    ALLEGRO_DISPLAY *display = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+
+    //inicializa o Allegro
+	if(!al_init()) {
+		fprintf(stderr, "failed to initialize allegro!\n");
+		return -1;
+	}
+
+    //cria uma tela com dimensoes de SCREEN_W, SCREEN_H pixels
+	display = al_create_display(SCREEN_W, SCREEN_H);
+	if(!display) {
+		fprintf(stderr, "failed to create display!\n");
+		// al_destroy_timer(timer);
+		return -1;
+	}
+
+    //cria a fila de eventos
+	event_queue = al_create_event_queue();
+	if(!event_queue) {
+		fprintf(stderr, "failed to create event_queue!\n");
+		al_destroy_display(display);
+		// al_destroy_timer(timer);
+		return -1;
+	}
+    //instala o teclado
+	if(!al_install_keyboard()) {
+		fprintf(stderr, "failed to install keyboard!\n");
+		return -1;
+	}
+	
+	//instala o mouse
+	if(!al_install_mouse()) {
+		fprintf(stderr, "failed to initialize mouse!\n");
+		return -1;
+	}
+
+    //registra na fila os eventos de tela (ex: clicar no X na janela)
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+    //registra na fila os eventos de teclado (ex: pressionar uma tecla)
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	//registra na fila os eventos de mouse (ex: clicar em um botao do mouse)
+	al_register_event_source(event_queue, al_get_mouse_event_source());  
+
+
+    int playing = 1;
+	while(playing) {
+
+        al_clear_to_color(al_map_rgb(0, 255, 255));
+        //atualiza a tela (quando houver algo para mostrar)
+	    al_flip_display();
+
+		ALLEGRO_EVENT ev;
+		//espera por um evento e o armazena na variavel de evento ev
+		al_wait_for_event(event_queue, &ev);
+
+        //se o tipo de evento for o fechamento da tela (clique no x da janela)
+		if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			playing = 0;
+		}
+
+        //se o tipo de evento for o fechamento da tela (clique no x da janela)
+		else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			playing = 0;
+		}
+		//se o tipo de evento for um clique de mouse
+		else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+			printf("\nmouse clicado em: %d, %d", ev.mouse.x, ev.mouse.y);
+		}
+		//se o tipo de evento for um pressionar de uma tecla
+		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+			//imprime qual tecla foi
+			printf("\ncodigo tecla: %d", ev.keyboard.keycode);
+		}
+    }
+
+    //procedimentos de fim de jogo (fecha a tela, limpa a memoria, etc)
+	
+	//al_destroy_timer(timer);
+	al_destroy_display(display);
+	al_destroy_event_queue(event_queue);
+
+    return 0;
+}
